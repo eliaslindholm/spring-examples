5144bb2c56fb5e721130d9c715c8ee75ad927f16
Elias Lindholm <elias.lindholm@gmail.com>
2012-06-17 13:52:37 +0200

    refactoring

--- spring-rest-app/src/test/java/elilin/spring/restapp/runner/MessageServiceTest.java
+++ spring-rest-app/src/test/java/elilin/spring/restapp/runner/MessageServiceTest.java
@@ -3,15 +3,20 @@ package elilin.spring.restapp.runner;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -21,6 +26,8 @@ import org.junit.Test;
 public class MessageServiceTest {
 	
 	private static WebAppRunner runner;
+	
+	private HttpClient httpclient = new DefaultHttpClient();
 
 	@BeforeClass
 	public static void startWebapp() throws Exception {
@@ -35,37 +42,30 @@ public class MessageServiceTest {
 	
 	@Test
 	public void afterPostingAMessageItCanBeRetrivedUsingTheProvidedLocation() throws Exception {
-		
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost post = new HttpPost(runner.getAbsoluteUrl("/messages"));
-		
-		post.setEntity(new StringEntity(new ObjectMapper().writeValueAsString(new MessageDto("Hello World!", "Foppa")), "application/json", "ISO-8859-1"));
+		HttpPost post = new HttpPost(getMessagesUri());
+		post.setEntity(new StringEntity(toJson(new MessageDto("Hello World!", "Foppa")), "application/json", "ISO-8859-1"));
 		HttpResponse response = httpclient.execute(post);
 		response.getEntity().getContent().close();
-		assertNotNull(response.getFirstHeader("Location"));
+		assertNotNull("Location header should be set", response.getFirstHeader("Location"));
+		assertEquals("Status code", HttpStatus.SC_CREATED, response.getStatusLine().getStatusCode());
 		
-		HttpGet get = new HttpGet(response.getFirstHeader("Location").getValue());
-		response = httpclient.execute(get);
-		
-		MessageDto result = new ObjectMapper().readValue(response.getEntity().getContent(), MessageDto.class);
+		response = httpclient.execute(new HttpGet(response.getFirstHeader("Location").getValue()));
+		MessageDto result = fromJson(response.getEntity().getContent(), MessageDto.class);
 		assertEquals("Hello World!", result.getMessage());
 		assertEquals("Foppa", result.getSignature());
 	}
-	
+
 	@Test
 	public void putUpdatesAnExistingMessage() throws Exception {
-		
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost post = new HttpPost(runner.getAbsoluteUrl("/messages"));
-		
-		post.setEntity(new StringEntity(new ObjectMapper().writeValueAsString(new MessageDto("Hello World!", "Foppa")), "application/json", "ISO-8859-1"));
+		HttpPost post = new HttpPost(getMessagesUri());
+		post.setEntity(new StringEntity(toJson(new MessageDto("Hello World!", "Foppa")), "application/json", "ISO-8859-1"));
 		HttpResponse response = httpclient.execute(post);
 		response.getEntity().getContent().close();
 		
 		String messageUri = response.getFirstHeader("Location").getValue();
 		HttpGet get = new HttpGet(messageUri);
 		response = httpclient.execute(get);
-		MessageDto msg = new ObjectMapper().readValue(response.getEntity().getContent(), MessageDto.class);
+		MessageDto msg = fromJson(response.getEntity().getContent(), MessageDto.class);
 		String etag = response.getFirstHeader("ETag").getValue();
 		response.getEntity().getContent().close();
 
@@ -77,11 +77,24 @@ public class MessageServiceTest {
 		
 		get = new HttpGet(messageUri);
 		response = httpclient.execute(get);
-		MessageDto updated = new ObjectMapper().readValue(response.getEntity().getContent(), MessageDto.class);
+		MessageDto updated = fromJson(response.getEntity().getContent(), MessageDto.class);
 		
 		assertEquals("Updated message", updated.getMessage());
 	}
 	
+	private <T> T fromJson(InputStream content, Class<T> type) throws Exception {
+		return new ObjectMapper().readValue(content, type);
+	}
+
+	private String toJson(Object o) throws IOException, JsonGenerationException,
+			JsonMappingException {
+		return new ObjectMapper().writeValueAsString(o);
+	}
+
+	private String getMessagesUri() {
+		return runner.getAbsoluteUrl("/messages");
+	}
+
 	static class MessageDto {
 		
 		private Long id;
@@ -130,11 +143,4 @@ public class MessageServiceTest {
 		
 	}
 	
-	static class MessageClient {
-		
-		public void createMessage() {
-			
-		}
-	}
-
 }
